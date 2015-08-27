667514b491916e09f03a6bb84553ffacc9f40963
King James Torres <tcorp.kingjames@gmail.com>
2015-08-27 15:59:13 +0800

    Added Namespaces

--- src/Kiann/NuSoap/Lib/nusoap.php
+++ src/Kiann/NuSoap/Lib/nusoap.php
@@ -160,11 +160,13 @@ class nusoap_base {
 	* @access   public
 	*/
 	var $namespaces = array(
+		'soap' => 'http://schemas.xmlsoap.org/soap/envelope/'
 		'SOAP-ENV' => 'http://schemas.xmlsoap.org/soap/envelope/',
 		'xsd' => 'http://www.w3.org/2001/XMLSchema',
 		'xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
-		'SOAP-ENC' => 'http://schemas.xmlsoap.org/soap/encoding/'
-		);
+		'SOAP-ENC' => 'http://schemas.xmlsoap.org/soap/encoding/',
+		'asmx' => 'http://hdi.com.mx/asmx/'
+	);
 
 	/**
 	* namespaces used in the current context, e.g. during serialization
